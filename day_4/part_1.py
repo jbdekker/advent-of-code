55913d5e6127a2caaa8b271473bcc487dbc8b35e
Jordi Dekker <47210270+jbdekker@users.noreply.github.com>
2022-12-05 10:59:00 +0100

    Update part_1.py

--- day_4/part_1.py
+++ day_4/part_1.py
@@ -5,32 +5,23 @@ fp = "input"
 
 
 def str_to_int_interval(interval: List[str]) -> List[int]:
-    # ['2', '4']
     return list(map(int, interval))
 
 
 def parse_pair(pair: List[str]) -> List[Tuple[int]]:
-    # pair: ['2-4', '6-8']
-    pair = [p.split("-") for p in pair]
-    return list(map(str_to_int_interval, pair))
+    return list(map(str_to_int_interval, [p.split("-") for p in pair]))
 
 
 def to_section_pairs(lines: List[str]) -> List[Tuple[Tuple[int]]]:
-    pairs = [l.split(",") for l in lines]
-    pairs = list(map(parse_pair, pairs))
-    return pairs
+    return list(map(parse_pair, [l.split(",") for l in lines]))
 
 
 def left_contains_right(a: List[int], b: List[int]) -> bool:
-    if a[0] <= b[0] and a[1] >= b[1]:
-        return True
-    return False
+    return a[0] <= b[0] and a[1] >= b[1]
 
 
 def one_contains_the_other(a: List[int], b: List[int]) -> bool:
-    if left_contains_right(a, b) or left_contains_right(b, a):
-        return True
-    return False
+    return left_contains_right(a, b) or left_contains_right(b, a):
 
 
 def has_overlap(a: List[int], b: List[int]) -> bool:
@@ -40,9 +31,8 @@ def has_overlap(a: List[int], b: List[int]) -> bool:
 with open(fp, "r") as f:
     lines = [l.strip("\n") for l in f.readlines()]
 
-pairs = to_section_pairs(lines)
-fully_contained = sum([one_contains_the_other(p[0], p[1]) for p in pairs])
-has_overlap = sum([has_overlap(p[0], p[1]) for p in pairs])
+n_fully_contained = sum([one_contains_the_other(*p) for p in to_section_pairs(lines)])
+n_has_overlap = sum([has_overlap(*p) for p in pairs])
 
-print(f"{fully_contained=}")
-print(f"{has_overlap=}")
+print(f"{n_fully_contained=}")
+print(f"{n_has_overlap=}")
