282ab0de1075dccb8498ae0cdcb7705f7026eb4a
jbdekker <me@jordidekker.net>
2023-12-01 16:06:10 +0100

    day_1_2

--- 2023/day_1/src/main.rs
+++ 2023/day_1/src/main.rs
@@ -5,7 +5,6 @@ use regex::Regex;
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
-
     for line in read_to_string(filename).unwrap().lines() {
         result.push(line.to_string())
     }
