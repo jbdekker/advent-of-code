b1b6c958747d0f0a1cf8fdff9bde4fa44924b501
jbdekker <me@jordidekker.net>
2023-12-06 00:08:14 +0100

    test works!

--- 2023/day-05/src/bin/part2.rs
+++ 2023/day-05/src/bin/part2.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 
 fn main() {
     let input = include_str!("input.txt");
@@ -8,47 +8,90 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct Range {
+struct MappingRule {
     destination_start: i64,
     source_start: i64,
     length: i64,
 }
 
-impl Range {
-    fn contains(&self, i: i64) -> bool {
-        (i >= self.source_start) && (i < self.source_start + self.length)
-    }
-
+impl MappingRule {
     fn map(&self, i: i64) -> i64 {
         i - self.source_start + self.destination_start
+    }
+
+    fn map_range(
+        &self,
+        range: std::ops::Range<i64>,
+    ) -> (Vec<std::ops::Range<i64>>, Vec<std::ops::Range<i64>>) {
+        // (matched, unmatched)
+        let source_end: i64 = self.source_start + self.length;
+
+        if range.start >= source_end || range.end <= self.source_start {
+            // No overlap!
+            (Vec::new(), Vec::from([range]))
+        } else if range.start < self.source_start && range.end > source_end {
+            //         |-------|
+            //     |---------------|
+            (
+                Vec::from([self.destination_start..self.map(source_end)]),
+                Vec::from([range.start..self.source_start, source_end..range.end]),
+            )
+        } else if range.start < self.source_start && range.end < source_end {
+            //         |--------|
+            //      |-------|
+            (
+                Vec::from([self.destination_start..self.map(range.end)]), // center bit
+                Vec::from([range.start..self.source_start]),              // left bit
+            )
+        } else if range.start > self.source_start && range.end > source_end {
+            //         |--------|
+            //              |-------|
+            (
+                Vec::from([self.map(range.start)..self.map(source_end)]), // center bit
+                Vec::from([source_end..range.end]),                       // right bit
+            )
+        } else {
+            //         |-----------------|
+            //              |-------|
+            (
+                Vec::from([self.map(range.start)..self.map(range.end)]),
+                Vec::new(),
+            )
+        }
     }
 }
 
 #[derive(Debug)]
 struct Map {
-    ranges: Vec<Range>,
+    mapping_rules: Vec<MappingRule>,
 }
 
 impl Map {
-    fn contains(&self, i: i64) -> bool {
-        self.ranges.iter().any(|r| r.contains(i))
-    }
+    fn map_range(&self, ranges: Vec<std::ops::Range<i64>>) -> Vec<std::ops::Range<i64>> {
+        let mut destination_ranges: Vec<std::ops::Range<i64>> = Vec::new();
 
-    fn map(&self, i: i64) -> Vec<i64> {
-        self.ranges
-            .iter()
-            .filter_map(|x| match x.contains(i) {
-                true => Some(x.map(i)),
-                false => None,
-            })
-            .collect()
+        let mut todo = ranges;
+        for rule in self.mapping_rules.iter() {
+            let mut unmatched_ranges: Vec<std::ops::Range<i64>> = Vec::new();
+            for range in todo.into_iter() {
+                let (mut matched, mut unmatched) = rule.map_range(range);
+
+                destination_ranges.append(&mut matched);
+                unmatched_ranges.append(&mut unmatched);
+            }
+            todo = unmatched_ranges;
+        }
+        destination_ranges.append(&mut todo.clone());
+
+        destination_ranges
     }
 }
 
 fn process(input: &str) -> i64 {
-    let mut seeds: BTreeSet<i64> = BTreeSet::new();
     let mut maps: BTreeMap<&str, Map> = BTreeMap::new();
     let mut last_name: &str = "";
+
+    let mut seed_ranges: Vec<std::ops::Range<i64>> = Vec::new();
 
     let _ = input
         .lines()
@@ -56,77 +99,81 @@ fn process(input: &str) -> i64 {
             let re = Regex::new(r"(seeds:)\s*(((\d+)\s*)+)").unwrap();
             if re.is_match(line) {
                 let caps = re.captures(line).unwrap();
-                let values = {
-                    let items = caps
-                        .get(2)
-                        .unwrap()
-                        .as_str()
-                        .split_whitespace()
-                        .map(|x| x.parse::<i64>().unwrap())
-                        .collect::<Vec<i64>>();
 
-                    let value_iter = items.chunks(2).map(|p| {
-                        let end: i64 = p[0] + p[1];
-                        (p[0]..end).map(i64::from).collect::<Vec<i64>>()
-                    });
+                let items = caps
+                    .get(2)
+                    .unwrap()
+                    .as_str()
+                    .split_whitespace()
+                    .map(|x| x.parse::<i64>().unwrap())
+                    .collect::<Vec<i64>>();
 
-                    value_iter.collect::<Vec<Vec<i64>>>().into_iter().flatten()
-                };
-                seeds = BTreeSet::from_iter(values);
-                println!("# seeds: {}", seeds.len());
+                for p in items.chunks(2) {
+                    seed_ranges.push(p[0]..(p[0] + p[1]));
+                }
             }
 
             let re: Regex = Regex::new(r"^(\S+) map:$").unwrap();
             if re.is_match(line) {
                 let caps = re.captures(line).unwrap();
                 last_name = caps.get(1).unwrap().as_str();
-                maps.insert(last_name, Map { ranges: Vec::new() });
+                maps.insert(
+                    last_name,
+                    Map {
+                        mapping_rules: Vec::new(),
+                    },
+                );
             }
 
             let re: Regex = Regex::new(r"^(\d+) (\d+) (\d+)$").unwrap();
             if re.is_match(line) {
                 let caps = re.captures(line).unwrap();
-                let range = Range {
+                let range = MappingRule {
                     destination_start: caps.get(1).unwrap().as_str().parse::<i64>().unwrap(),
                     source_start: caps.get(2).unwrap().as_str().parse::<i64>().unwrap(),
                     length: caps.get(3).unwrap().as_str().parse::<i64>().unwrap(),
                 };
-                maps.entry(last_name).and_modify(|x| x.ranges.push(range));
+                maps.entry(last_name)
+                    .and_modify(|x| x.mapping_rules.push(range));
             }
             None
         })
         .collect::<Vec<&str>>();
 
-    fn get_mapping(mapping: &BTreeMap<&str, Map>, key: &str, ids: Vec<i64>) -> Vec<i64> {
+    fn apply_mapping(
+        mapping: &BTreeMap<&str, Map>,
+        key: &str,
+        ranges: Vec<std::ops::Range<i64>>,
+    ) -> Vec<std::ops::Range<i64>> {
         let map: &Map = mapping.get(key).unwrap();
 
-        ids.into_iter()
-            .map(|i| {
-                if map.contains(i) {
-                    map.map(i)
-                } else {
-                    Vec::from([i])
-                }
-            })
-            .flatten()
-            .collect()
+        map.map_range(ranges)
     }
 
-    let soils: Vec<i64> = get_mapping(&maps, "seed-to-soil", seeds.into_iter().collect());
-    // println!("soils: {soils:?}");
-    let fertilizers: Vec<i64> = get_mapping(&maps, "soil-to-fertilizer", soils);
+    let soils: Vec<std::ops::Range<i64>> = apply_mapping(&maps, "seed-to-soil", seed_ranges);
+    println!("soils: {soils:?}");
+    let fertilizers: Vec<std::ops::Range<i64>> = apply_mapping(&maps, "soil-to-fertilizer", soils);
     // println!("fertilizers: {fertilizers:?}");
-    let waters: Vec<i64> = get_mapping(&maps, "fertilizer-to-water", fertilizers);
+    let waters: Vec<std::ops::Range<i64>> =
+        apply_mapping(&maps, "fertilizer-to-water", fertilizers);
     // println!("waters: {waters:?}");
-    let lights: Vec<i64> = get_mapping(&maps, "water-to-light", waters);
+    let lights: Vec<std::ops::Range<i64>> = apply_mapping(&maps, "water-to-light", waters);
     // println!("lights: {lights:?}");
-    let temperatures: Vec<i64> = get_mapping(&maps, "light-to-temperature", lights);
+    let temperatures: Vec<std::ops::Range<i64>> =
+        apply_mapping(&maps, "light-to-temperature", lights);
     // println!("temperatures: {temperatures:?}");
-    let humidities: Vec<i64> = get_mapping(&maps, "temperature-to-humidity", temperatures);
+    let humidities: Vec<std::ops::Range<i64>> =
+        apply_mapping(&maps, "temperature-to-humidity", temperatures);
     // println!("humidities: {humidities:?}");
-    let locations: Vec<i64> = get_mapping(&maps, "humidity-to-location", humidities);
+    let locations: Vec<std::ops::Range<i64>> =
+        apply_mapping(&maps, "humidity-to-location", humidities);
     // println!("locations: {locations:?}");
-    *locations.iter().min().unwrap()
+
+    locations
+        .into_iter()
+        .map(|range| range.start)
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -136,7 +183,7 @@ mod tests {
     #[test]
     fn it_works() {
         let result = process(
-            "seeds: 79 14 55 13
+            "seeds: 98 14 55 13
 
 seed-to-soil map:
 50 98 2
