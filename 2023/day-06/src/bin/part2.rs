18d5147265642cbee6b3b1e700b6ed139f7b7e92
Jordi Dekker <jordi.dekker@shell.com>
2023-12-06 10:34:39 +0100

    part 2!

--- 2023/day-06/src/bin/part2.rs
+++ 2023/day-06/src/bin/part2.rs
@@ -1,11 +1,35 @@
+use itertools::Itertools;
+use std::ops::RangeInclusive;
 fn main() {
     let input = include_str!("input.txt");
     let output = process(input);
     dbg!(output);
 }
 
-fn process(input: &str) -> String {
-    "4".to_string()
+fn process(input: &str) -> i64 {
+    let (time_limits, distance_records) = input.lines().into_iter().map(|line| {
+        line.trim().split(":").skip(1).map(|num| {
+            num.replace(" ", "").parse::<i64>().expect("no es bueno!")
+        }
+        ).collect::<Vec<i64>>()
+    }).collect_tuple().unwrap();
+
+    fn abc_limits(a: i64, b: i64, c: i64) -> RangeInclusive<i64> {
+        let a = a as f64;
+        let b = b as f64;
+        let c = c as f64;
+
+        let (lower, upper) = (
+            ((-b + (b*b - 4.0*a*c).sqrt()) / (2.0 * a)) as i64 + 1,
+            ((-b - (b*b - 4.0*a*c).sqrt()) / (2.0 * a)).ceil() as i64 - 1,
+        );
+        lower..=upper
+    }
+
+    let product_of_ways = (0..time_limits.len()).into_iter().map(|i: usize| {
+        abc_limits(-1, time_limits[i], -distance_records[i]).try_len().expect("no es bueno!") as i64
+    }).reduce(|a, b| a * b).unwrap();
+    product_of_ways
 }
 
 #[cfg(test)]
@@ -14,7 +38,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = process("");
-        assert_eq!(result, "4".to_string())
+        let result = process("Time:      7  15   30
+Distance:  9  40  200");
+        assert_eq!(result, 71503)
     }
 }
