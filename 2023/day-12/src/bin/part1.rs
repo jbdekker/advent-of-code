81c9203b2a99e842d8d5a3c120bb7ac9903c05a9
Jordi Dekker <me@jordidekker.net>
2023-12-12 17:44:55 +0100

    should work

--- 2023/day-12/src/bin/part1.rs
+++ 2023/day-12/src/bin/part1.rs
@@ -5,54 +5,75 @@ fn main() {
 }
 
 fn number_of_options(record: &str, nums: &Vec<usize>) -> usize {
+    dbg!(&record);
+    dbg!(&nums);
+
+    // if dbg!(record.len() + 1) < dbg!(nums.iter().sum::<usize>() + nums.len()) {
+    //     println!("record is too short");
+    //     return 0;
+    // }
+
     if record.len() == 0 {
         match nums.is_empty() {
-            true => return 1,
-            false => return 0,
+            true => return dbg!(1),
+            false => return dbg!(0),
         };
     }
 
     if nums.is_empty() {
         match record.contains('#') {
-            true => return 0,
-            false => return 1,
+            true => return dbg!(0),
+            false => return dbg!(1),
         };
     }
 
     let mut result = 0;
 
-    let next_char = record.chars().nth(0).unwrap();
+    let next_char = record.chars().next().unwrap();
     if ['.', '?'].into_iter().any(|s| s == next_char) {
+        println!("Next char: {}", next_char);
         result += number_of_options(&record[1..], &nums)
     }
 
     if ['#', '?'].into_iter().any(|s| s == next_char) {
-            // must be enough springs left
-            if record.len() >= nums[0] && !record[..nums[0]].contains('.') && (record.len() == nums[0] || record.chars().nth(nums[0]).unwrap() != '#') {
-                result += number_of_options(&record[nums[0]+1..], *nums.clone()[1..]);    
+        // must be enough springs left
+        if record.len() >= nums[0] && !record[..nums[0]].contains('.') {
+            if record.len() == nums[0] {
+                println!("record: {}, nums: {:?}", record, nums);
+                result += number_of_options(&record[nums[0]..], &nums[1..].to_vec());
+            } else {
+                result += number_of_options(&record[(nums[0] + 1)..], &nums[1..].to_vec());
             }
-                result += 0;
         }
+    }
 
     return result;
 }
 
 fn process(input: &str) -> usize {
-    let result: usize = input.lines().into_iter().map(|line| {
-        let parts = line.split_whitespace().collect::<Vec<_>>();
-        
-        let record = parts[0];
-        let numbers: Vec<_> = parts[1].split(',').map(|x| x.parse::<usize>().unwrap()).collect();
+    let result: usize = input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let parts = line.split_whitespace().collect::<Vec<_>>();
 
-        let options = number_of_options(&record, &numbers);
+            let record = parts[0];
+            let numbers: Vec<_> = parts[1]
+                .split(',')
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect();
 
-        dbg!(&record);
-        dbg!(&numbers);
+            let options = number_of_options(&record, &numbers);
 
-        options
-    }).sum();
+            // dbg!(&record);
+            // dbg!(&numbers);
 
-    todo!();
+            dbg!(options)
+        })
+        .sum();
+
+    return result;
+    // todo!();
 }
 
 #[cfg(test)]
@@ -61,12 +82,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = process("???.### 1,1,3
-        .??..??...?##. 1,1,3
-        ?#?#?#?#?#?#?#? 1,3,1,6
-        ????.#...#... 4,1,1
-        ????.######..#####. 1,6,5
-        ?###???????? 3,2,1");
-        assert_eq!(result, 21)
+        let result = process("?###???????? 3,2,1");
+        assert_eq!(result, 10)
     }
 }
