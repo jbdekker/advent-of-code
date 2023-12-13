1338d535e01b1437b0b694277303371f7b7e3837
Jordi Dekker <me@jordidekker.net>
2023-12-13 23:37:22 +0100

    part 1 cleaned-up

--- 2023/day-13/src/bin/part1.rs
+++ 2023/day-13/src/bin/part1.rs
@@ -1,88 +1,74 @@
-use itertools::Itertools;
-use itertools::EitherOrBoth::Both;
-use std::collections::HashSet;
-
 fn main() {
     let input = include_str!("input.txt");
     let output = process(input);
     dbg!(output);
 }
 
-fn find_line_reflections(line: &str, idx: usize, seen: &mut Vec<usize>) -> () {
-    if idx > line.len() - 1 {
-        return; // done
-    }
-
-    let left = line[..idx].chars().rev();
-    let right = &line[idx..];
-    let matched: Vec<bool> = left.zip_longest(right.chars()).filter_map(|r| {
-        match r {
-            Both(l, r) => match l == r {true => Some(true), false => Some(false),},
-            _ => None,
-        }
-    }).collect();
-
-    if matched.iter().all(|x| *x) && matched.len() > 0 {
-        seen.push(idx);
-    }
-
-    return find_line_reflections(line, idx+1, seen);
+fn find_folds(block: &Vec<Vec<char>>) -> Option<usize> {
+    block[0]
+        .iter()
+        .zip(block[0].iter().skip(1))
+        .enumerate()
+        .filter_map(|(i, (a, b))| match a == b {
+            true => try_fold(block, 0, i + 1),
+            false => None,
+        })
+        .next()
 }
 
-fn find_block_reflection(block: &Vec<String>, find_horizontal: bool) -> Option<usize> {
+fn try_fold(block: &Vec<Vec<char>>, line_idx: usize, col_idx: usize) -> Option<usize> {
+    let res = block[line_idx][..col_idx]
+        .iter()
+        .rev()
+        .zip(block[line_idx][col_idx..].iter())
+        .all(|(a, b)| a == b);
 
-    let transposed_block = transpose(block);
-    let block = match find_horizontal {true => &transposed_block, false => block};
+    match res {
+        true => match line_idx == block.len() - 1 {
+            true => Some(col_idx),
+            false => try_fold(block, line_idx + 1, col_idx),
+        },
+        false => None,
+    }
+}
 
-    let mut line_seen: Vec<Vec<usize>> = Vec::new();
-    let _reflections: Vec<_> = block.iter().map(|line| {
-        let mut seen: Vec<usize> = Vec::new();
-        find_line_reflections(line, 1, &mut seen);
-        line_seen.push(seen);
-    }).collect();
+fn find(block: &Vec<Vec<char>>) -> usize {
+    match find_folds(block) {
+        Some(v) => v,
+        None => {
+            let block_transposed = (0..block[0].len())
+                .map(|col| (0..block.len()).map(|row| block[row][col]).collect())
+                .collect();
 
-    let sets: Vec<HashSet<_>> = line_seen.iter()
-        .map(|list| list.iter().cloned().collect())
-        .collect();
-
-    for &num in &sets[0] {
-        if sets.iter().all(|s| s.contains(&num)) {
-            match find_horizontal {
-                true => return Some(100 * num),
-                false => return Some(num),
+            match find_folds(&block_transposed) {
+                Some(v) => 100 * v,
+                None => panic!("No fold found!"),
             }
         }
     }
-
-    return None
-
-}
-
-fn transpose(input: &Vec<String>) -> Vec<String> {
-    (0..input[0].len()).map(|i| {
-        input.iter()
-            .map(|s| s.chars().nth(i).unwrap_or(' ')) // Replace `unwrap_or(' ')` with another character if needed
-            .collect()
-    }).collect()
 }
 
 fn process(input: &str) -> usize {
     let blocks: Vec<_> = input.split("\n\n").collect();
 
-    let result: usize = blocks.iter().map(|block| {
-        let lines: Vec<String> = block.lines().filter_map(|line| {
-            let line = line.trim();
-            
-            match line {
-                "" => None,
-                _ => Some(line.to_string())
-            }
-        }).collect();
+    let result: usize = blocks
+        .iter()
+        .map(|block| {
+            let block: Vec<Vec<char>> = block
+                .lines()
+                .filter_map(|line| {
+                    let line = line.trim();
 
-        let res = vec![true, false].into_iter().filter_map(|v| find_block_reflection(&lines,v)).collect::<Vec<_>>();
+                    match line {
+                        "" => None,
+                        _ => Some(line.chars().collect()),
+                    }
+                })
+                .collect();
 
-        res[0]
-    }).sum();
+            find(&block)
+        })
+        .sum();
 
     result
 }
@@ -93,7 +79,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = process("#.##..##.
+        let result = process(
+            "#.##..##.
 ..#.##.#.
 ##......#
 ##......#
@@ -107,7 +94,8 @@ mod tests {
 #####.##.
 #####.##.
 ..##..###
-#....#..#");
+#....#..#",
+        );
         assert_eq!(result, 405)
     }
 }
