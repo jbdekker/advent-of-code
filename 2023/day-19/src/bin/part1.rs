c0223db0763d946d4d8afe394f7c22dfeb67d945
Jordi Dekker <me@jordidekker.net>
2023-12-18 19:41:13 +0100

    setup

---
+++ 2023/day-19/src/bin/part1.rs
@@ -0,0 +1,45 @@
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> usize {
+    let (instructions, parts) = input.split("\n\n").collect_tuple().unwrap();
+
+    dbg!(&instructions);
+    dbg!(&parts);
+
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = process(
+            "px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}",
+        );
+        assert_eq!(result, 4)
+    }
+}
