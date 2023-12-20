d988d1a39686ccec9d5940c1007b0471be0456ec
jbdekker <me@jordidekker.net>
2023-12-20 11:25:13 +0100

    backup

--- 2023/day-20/src/bin/part1.rs
+++ 2023/day-20/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque};
 
 use itertools::Itertools;
 
@@ -8,7 +8,20 @@ fn main() {
     dbg!(output);
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq, Copy)]
+enum Pulse {
+    High,
+    Low,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum State {
+    On,
+    Off,
+    None,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum ModuleType {
     Button,
     Broadcaster,
@@ -16,11 +29,44 @@ enum ModuleType {
     Conjunction,
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct Module {
     mtype: ModuleType,
     name: String,
     destinations: Vec<String>,
+    state: State,
+    history: BTreeMap<String, Pulse>,
+}
+
+impl Module {
+    fn process(&mut self, pulse: Pulse, origin: String) -> Option<(Vec<String>, Pulse)> {
+        match self.mtype {
+            ModuleType::Broadcaster => Some((self.destinations.clone(), pulse)),
+            ModuleType::Button => Some((self.destinations.clone(), Pulse::Low)),
+            ModuleType::Conjunction => {
+                self.history.insert(origin, pulse);
+                match self.history.values().all(|v| *v == Pulse::High) {
+                    true => Some((self.destinations.clone(), Pulse::Low)),
+                    false => Some((self.destinations.clone(), Pulse::Low)),
+                }
+            }
+            ModuleType::FlipFlop => match pulse {
+                Pulse::High => None,
+                Pulse::Low => match self.state {
+                    State::Off => {
+                        self.state = State::On;
+                        Some((self.destinations.clone(), Pulse::High))
+                    }
+                    State::On => {
+                        self.state = State::Off;
+                        Some((self.destinations.clone(), Pulse::Low))
+                    }
+                    State::None => None,
+                },
+            },
+            _ => panic!(),
+        }
+    }
 }
 
 fn process(input: &str) -> usize {
@@ -35,7 +81,7 @@ fn process(input: &str) -> usize {
             let x = m.trim();
             match x.trim() {
                 x if x == "broadcaster" => (x.to_string(), ModuleType::Broadcaster), // broadcaster
-                x if x.contains("%") => (x.replace("&", ""), ModuleType::FlipFlop),  // flip-flop
+                x if x.contains("%") => (x.replace("%", ""), ModuleType::FlipFlop),  // flip-flop
                 x if x.contains("&") => (x.replace("&", ""), ModuleType::Conjunction), // conjunction
                 _ => panic!("Should be unreachable, got {}", m.trim()),
             }
@@ -47,21 +93,49 @@ fn process(input: &str) -> usize {
         .map(|d| d.split(",").map(|x| x.trim().to_string()).collect())
         .collect();
 
-    let configurations: BTreeMap<String, Module> = module_name
-        .iter()
-        .zip(&module_type)
-        .zip(&destinations)
-        .map(|((name, mtype), destination)| {
-            (
-                name.clone(),
-                Module {
-                    mtype: mtype.clone(),
-                    name: name.clone(),
-                    destinations: destination.clone(),
-                },
-            )
-        })
-        .collect();
+    let mut modules: Vec<Module> = Vec::new();
+    modules.push(Module {
+        name: "button".to_string(),
+        mtype: ModuleType::Button,
+        destinations: vec!["broadcaster".to_string()],
+        history: BTreeMap::new(),
+        state: State::None,
+    });
+
+    let mut configurations: BTreeMap<String, &mut Module> = BTreeMap::new();
+
+    for ((name, mtype), destination) in module_name.iter().zip(&module_type).zip(&destinations) {
+        modules.push(Module {
+            mtype: mtype.clone(),
+            name: name.clone(),
+            destinations: destination.clone(),
+            state: match mtype {
+                ModuleType::FlipFlop => State::Off,
+                _ => State::None,
+            },
+            history: BTreeMap::new(),
+        });
+
+        configurations.insert(name.clone(), &modules.last().unwrap());
+    }
+
+    // init the conjunctions
+    for (name, module) in configurations.iter_mut() {
+        for destination in module.destinations.iter() {
+            if configurations.get(destination).unwrap().mtype == ModuleType::Conjunction {
+                configurations
+                    .entry(destination.to_string())
+                    .and_modify(|f| {
+                        f.history.insert(name.clone(), Pulse::Low);
+                    });
+            }
+        }
+    }
+
+    dbg!(&configurations);
+
+    let mut queue: VecDeque<&Module> = VecDeque::new();
+    // loop {}
 
     todo!();
 }
