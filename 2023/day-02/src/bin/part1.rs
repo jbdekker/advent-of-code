bf25252f0a4253b076b7fca2ec5a2c8f7fa2c5cb
Jordi Dekker <jordi.dekker@shell.com>
2024-01-09 15:19:45 +0100

    cleanup part1

--- 2023/day-02/src/bin/part1.rs
+++ 2023/day-02/src/bin/part1.rs
@@ -1,79 +1,33 @@
-use std::collections::HashMap;
-
 fn main() {
     let input = include_str!("input.txt");
     let output = process(input);
     dbg!(output);
 }
 
-fn process(input: &str) -> i32 {
-    let bag_contents: HashMap<&str, i32> =
-        HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
-
-    let sum_of_game_numbers: i32 = input
+fn process(input: &str) -> usize {
+    let sum_of_game_numbers: usize = input
         .lines()
-        .filter_map(|line| {
-            let parts: Vec<&str> = line.split(":").collect();
+        .enumerate()
+        .filter_map(|(game_id, line)| {
+            let mut rgb = vec![0, 0, 0];
 
-            let game: i32 = parts[0].trim().split(" ").collect::<Vec<&str>>()[1]
-                .parse::<i32>()
-                .expect("Unable to parse game #");
-
-            let grabs: Vec<Vec<&str>> = parts[1]
+            line.splitn(2, ":")
+                .nth(1)
+                .unwrap()
                 .split(&[',', ';'][..])
-                .map(|x| x.trim().split_whitespace().collect())
-                .collect();
+                .all(|cubes| {
+                    let parts: Vec<&str> = cubes.trim().split_whitespace().collect();
+                    let i = match parts[1] {
+                        "red" => 0,
+                        "green" => 1,
+                        "blue" => 2,
+                        _ => panic!(),
+                    };
 
-            let reds: i32 = grabs
-                .iter()
-                .filter_map(|x| {
-                    if x[1] == "red" {
-                        Some(x[0].parse::<i32>().unwrap())
-                    } else {
-                        None
-                    }
+                    rgb[i] = rgb[i].max(parts[0].parse::<usize>().unwrap());
+                    rgb[i] <= 12 + i
                 })
-                .max()
-                .unwrap();
-
-            let greens: i32 = grabs
-                .iter()
-                .filter_map(|x| {
-                    if x[1] == "green" {
-                        Some(x[0].parse::<i32>().unwrap())
-                    } else {
-                        None
-                    }
-                })
-                .max()
-                .unwrap();
-
-            let blues: i32 = grabs
-                .iter()
-                .filter_map(|x| {
-                    if x[1] == "blue" {
-                        Some(x[0].parse::<i32>().unwrap())
-                    } else {
-                        None
-                    }
-                })
-                .max()
-                .unwrap();
-
-            dbg!(&grabs);
-            if reds > *bag_contents.get("red").unwrap() {
-                return None;
-            }
-
-            if greens > *bag_contents.get("green").unwrap() {
-                return None;
-            }
-
-            if blues > *bag_contents.get("blue").unwrap() {
-                return None;
-            }
-
-            Some(game)
+                .then_some(game_id + 1)
         })
         .sum();
 
